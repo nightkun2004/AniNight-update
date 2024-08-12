9d62bafe1a2174dc62a9334a1ae0a20baba2431b
Nightkun <nthe7610@gmail.com>
2024-08-13 05:55:12 +0700

    AniNight

--- Middlewares/authorizeMiddleware.js
+++ Middlewares/authorizeMiddleware.js
@@ -9,27 +9,16 @@ const authorizeRole = (...role) => {
 };
 
 
-const authorizeRoleAdmin = (...roles) => {
+const authhorizeRoleAdmin = (...roles) => {
     return (req, res, next) => {
-        // ตรวจสอบว่าเข้าผ่าน subdomain หรือ path /admin
-        const isAdminPath = req.originalUrl.startsWith('/admin') || req.hostname.startsWith('admin.');
-        
-        // ดึงข้อมูล role จาก session และคุกกี้
-        const sessionRole = req.session.userlogin && req.session.userlogin.user && req.session.userlogin.user.role;
-        const cookieRole = req.cookies.token;
-
-        // ตรวจสอบ role จาก session หรือจากคุกกี้
-        const hasValidRole = roles.includes(sessionRole) || roles.includes(cookieRole);
-
-        if (isAdminPath && hasValidRole) {
+        if (req.session.userlogin && req.session.userlogin.user && roles.includes(req.session.userlogin.user.role)) {
             next();
         } else {
             res.status(403).render('./errors/admin/403', { message: 'คุณไม่ได้รับอนุญาตให้เข้าหน้านี้' });
         }
     };
-};
+}
 
 
 
-
-module.exports = {authorizeRole, authorizeRoleAdmin};
+module.exports = {authorizeRole, authhorizeRoleAdmin};
