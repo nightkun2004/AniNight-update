59150d16e7202c16c1c3f851c4300f1bb2f24e09
Nightkun <nthe7610@gmail.com>
2024-08-30 13:29:13 +0700

    AniNight

--- public/js/read.js
+++ public/js/read.js
@@ -262,26 +262,6 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    window.fbAsyncInit = function () {
-        FB.init({
-            appId: '1075150200774995',
-            status: true,
-            cookie: true,
-            xfbml: true,
-            version: 'v20.0'
-        });
-
-        FB.AppEvents.logPageView();
-    };
-
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/en_US/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
     // Share to social platforms
     shareButtons.forEach(button => {
         button.addEventListener('click', function () {
