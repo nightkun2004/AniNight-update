50dd938e0faa7cf30c473d9ed135058f8823deb9
Nightkun <nthe7610@gmail.com>
2024-11-09 23:12:16 +0700

    AniNight

--- Controllers/TermController.js
+++ Controllers/TermController.js
@@ -11,15 +11,43 @@ const Term = async (req,res) => {
         })
     } catch (error) {
         const errorMessage = error.message || 'Internal Server Error';
-        res.status(500).render('./th/index', {
-            error: errorMessage,
+        res.status(500).json({ msg: errorMessage })
+    }
+}
+
+
+const getPrivacyPolicy = async (req,res) => {
+    const userID = req.session.userlogin;
+    const lang = res.locals.lang;
+    try {
+        res.render("./th/PrivacyPolicy", {
             userID,
             lang ,
             translations: req.translations, 
-        });
+        })
+    } catch (error) {
+        const errorMessage = error.message || 'Internal Server Error';
+        res.status(500).json({ msg: errorMessage })
+    }
+}
+
+const getTermsService = async (req,res) => {
+    const userID = req.session.userlogin;
+    const lang = res.locals.lang;
+    try {
+        res.render("./th/TermsService", {
+            userID,
+            lang ,
+            translations: req.translations, 
+        })
+    } catch (error) {
+        const errorMessage = error.message || 'Internal Server Error';
+        res.status(500).json({ msg: errorMessage })
     }
 }
 
 module.exports = {
-    Term
+    Term,
+    getPrivacyPolicy,
+    getTermsService
 }
